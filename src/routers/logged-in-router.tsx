6941fb880d2b323965cf37e747a5fce747d7aa72
promoted.go@daamdaam.co.kr <promoted.go@gmail.com>
2021-05-10 16:16:07 +0900

    FRONTEND SETUP

---
+++ src/routers/logged-in-router.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const LoggedInRoter = () => <span>Logged In</span>;
---
+++ src/routers/logged-out-router.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const LoggedOutRoter = () => <span>Logged Out</span>;
